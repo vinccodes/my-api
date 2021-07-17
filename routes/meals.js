1ee968f8274d4eadaab9018738fa6187851e88d7
vinccodes <vinccodes@gmail.com>
2021-07-16 23:40:09 -0400

    Worked on RUD

--- routes/meals.js
+++ routes/meals.js
@@ -12,8 +12,8 @@ router.get('/', async(req, res)=> {
     }
 })
 // Get One
-router.get('/:id', async(req, res)=>{
-    res.send(req.params.id)
+router.get('/:id', getMeal, async(req, res)=>{
+    res.send(res.meal)
 })
 
 // Create One
@@ -25,20 +25,56 @@ router.post('/', async(req, res)=>{
         })
 
         const result = await meal.save();
-        res.json(result);
+        res.status(201).json(result);
     }
     catch(err) {
         res.status(400).json({ message: err.message })
     }
 })
-// Update One
-router.patch('/', (req, res)=>{
+// Update One // 
+router.patch('/:id', getMeal, async(req, res)=>{
+    if (req.body.name !== null ) {
+        res.meal.name = req.body.name
+    }
+    if (req.body.items != null) {
+        res.meal.items = req.body.items
+    }
+    try {
+        const updatedMeal = await res.meal.save()
+        res.json({ message: 'Updated meal' })
 
+    } catch(err) {
+        res.status()
+    }
 })
 
 // Delete One
-router.delete('', (req, res)=>{
-
+router.delete('/:id', (req, res)=>{
+    try {
+        await Meal.findByIdAndDelete(req.params.id)
+        res.json({ message: "Deleted meal"})
+    } catch(err) {
+        res.status(500).json({ message: err.message })
+    }
 })
+
+// middleware function to get meal from database
+async function getMeal(req, res , next) {
+    let meal
+    try {
+        // get meal 
+         meal = await Meal.findById(req.params.id);
+        // return error json response
+        if (!meal) {
+            return res.status(400).json({message: "Error retrieving meal"})
+        }
+    }
+    catch (err){
+        return res.status(500).json({ message: err.message })
+    }
+
+    res.meal = meal
+    next()
+}
 
 module.exports = router;
